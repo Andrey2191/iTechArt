bdd18db48d7b3e6addaf21f631183ba166fdd2b2
Andrei Dashko <Andrei.Dashko@itechart-group.com>
2021-10-01 16:16:39 +0300

    add menu

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const subscribeBtn = document.querySelector(".subscribe__button");
 const inputName = document.querySelector(".input-name");
 const inputEmail = document.querySelector(".input-email");
+const menu = document.querySelector(".nav-btn");
+const menuLinks = document.querySelector(".nav__links");
 
 subscribeBtn.addEventListener("click", () => {
   if (inputName.value === "" && inputEmail.value === "") {
@@ -13,3 +15,9 @@ subscribeBtn.addEventListener("click", () => {
   inputName.value = "";
   inputEmail.value = "";
 });
+
+menu.addEventListener("click", () => {
+  menuLinks.classList.toggle("active");
+});
+
+console.log(menu);
